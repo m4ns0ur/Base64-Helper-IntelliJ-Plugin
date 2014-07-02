e08f96068dba66793434d51875a6c82fafe92b4c
m.rahimi <rahimi.mnr@gmail.com>
2014-07-02 15:08:13 +0200

    - test unit fix - some intellij stuff ignorance

--- src/com/mnr/java/intellij/idea/plugin/base64helper/test/AbstractpopupItemTest.java
+++ src/com/mnr/java/intellij/idea/plugin/base64helper/test/AbstractpopupItemTest.java
@@ -1,7 +1,46 @@
 package com.mnr.java.intellij.idea.plugin.base64helper.test;
 
+import com.mnr.java.intellij.idea.plugin.base64helper.*;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
 /**
  * @author m.rahimi
  */
-public class AbstractpopupItemTest {
+public class AbstractPopupItemTest extends AbstractTestData {
+    @Test
+    public void testEncodeDecode() throws Exception {
+        EncoderPopupItem encoderPopupItem = new EncoderPopupItem();
+        assertNotNull(encoderPopupItem);
+        assertTrue(encoderPopupItem.isSelectable(BASE64_STRING_1));
+        assertTrue(encoderPopupItem.isSelectable(TEST_STRING));
+        String base64String = encoderPopupItem.encodeDecode(TEST_STRING);
+        assertNotNull(base64String);
+        assertEquals(BASE64_STRING_1, base64String);
+
+        DecoderPopupItem decoderPopupItem = new DecoderPopupItem();
+        assertNotNull(decoderPopupItem);
+        assertTrue(decoderPopupItem.isSelectable(BASE64_STRING_1));
+        assertFalse(decoderPopupItem.isSelectable(TEST_STRING));
+        String textString = decoderPopupItem.encodeDecode(BASE64_STRING_1);
+        assertNotNull(textString);
+        assertEquals(TEST_STRING, textString);
+
+        HexEncoderPopupItem hexEncoderPopupItem = new HexEncoderPopupItem();
+        assertNotNull(hexEncoderPopupItem);
+        assertTrue(hexEncoderPopupItem.isSelectable(HEX_TEST_STRING));
+        assertFalse(hexEncoderPopupItem.isSelectable(TEST_STRING));
+        base64String = hexEncoderPopupItem.encodeDecode(HEX_TEST_STRING);
+        assertNotNull(base64String);
+        assertEquals(BASE64_STRING_2, base64String);
+
+        HexDecoderPopupItem hexDecoderPopupItem = new HexDecoderPopupItem();
+        assertNotNull(hexDecoderPopupItem);
+        assertTrue(hexDecoderPopupItem.isSelectable(BASE64_STRING_1));
+        assertFalse(hexDecoderPopupItem.isSelectable(TEST_STRING));
+        String hexString = hexDecoderPopupItem.encodeDecode(BASE64_STRING_2);
+        assertNotNull(hexString);
+        assertEquals("0" + HEX_TEST_STRING.toUpperCase(), hexString);
+    }
 }
