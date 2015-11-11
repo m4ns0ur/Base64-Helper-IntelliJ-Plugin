d74a5dc06c656375960b3005d7e6a06f5e6340c6
m.rahimi <rahimi.mnr@gmail.com>
2015-11-11 10:57:51 +0100

    get back to use JDK 6, to be compatible with older projects

--- src/com/mnr/java/intellij/idea/plugin/base64helper/MainActionHandler.java
+++ src/com/mnr/java/intellij/idea/plugin/base64helper/MainActionHandler.java
@@ -155,26 +155,34 @@ public class MainActionHandler extends EditorAction {
             return;
         }
 
-        CommandProcessor.getInstance().executeCommand(openProjects[0], () -> ApplicationManager.getApplication().runWriteAction(() -> {
-            if (editor == null || !editor.getDocument().isWritable()) {
-                return;
+        CommandProcessor.getInstance().executeCommand(openProjects[0], new Runnable() {
+            @Override
+            public void run() {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (editor == null || !editor.getDocument().isWritable()) {
+                            return;
+                        }
+
+                        final String selectedText = getEditorSelectedText(editor);
+
+                        if ((selectedText != null) && !selectedText.isEmpty()) {
+                            String encodeDecode = popupItem.encodeDecode(selectedText);
+
+                            if (encodeDecode != null) {
+                                encodeDecode = encodeDecode.replace('\r', '\0');
+                                EditorModificationUtil.insertStringAtCaret(editor, encodeDecode, true, true);
+                            }
+                        }
+                    }
+                });
             }
-
-            final String selectedText = getEditorSelectedText(editor);
-
-            if ((selectedText != null) && !selectedText.isEmpty()) {
-                String encodeDecode = popupItem.encodeDecode(selectedText);
-
-                if (encodeDecode != null) {
-                    encodeDecode = encodeDecode.replace('\r', '\0');
-                    EditorModificationUtil.insertStringAtCaret(editor, encodeDecode, true, true);
-                }
-            }
-        }), "Base64 Converter", null);
+        }, "Base64 Converter", null);
     }
 
     private List<PopupItem> getPopupItems() {
-        List<PopupItem> popupItemList = new ArrayList<>();
+        List<PopupItem> popupItemList = new ArrayList<PopupItem>();
         popupItemList.add(new EncoderPopupItem());
         popupItemList.add(new DecoderPopupItem());
         popupItemList.add(new HexEncoderPopupItem());
